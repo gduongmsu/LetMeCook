91eab352ef652e6724d4328b5a25138e2e8f481f
itsyourGOAT <123419631+itsyourGOAT@users.noreply.github.com>
2025-09-25 14:33:07 -0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import recipesRouter from "./routes/recipes.js";
 
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/recipes", recipesRouter);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Backend on http://localhost:${PORT}`));
