43494a101e1df981cd5c0d33739ac02a554dbd6d
Strongic <lstrong8@msudenver.edu>
2025-10-02 15:34:47 -0600

    fixed nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from './pages/Home.jsx'
+import NavigationBar from './components/NavigationBar.jsx'
 import { Routes, Route } from 'react-router-dom'
 import './css/App.css'
 
 function App() {
   return (
     <div>
+      <NavigationBar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
