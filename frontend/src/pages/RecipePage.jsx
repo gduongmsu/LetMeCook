0268584ea9d93154aaa35f2730ee01acfe90f916
Strongic <lstrong8@msudenver.edu>
2025-10-16 13:41:19 -0600

    returning same data from RecipeById.jsx

--- frontend/src/pages/RecipePage.jsx
+++ frontend/src/pages/RecipePage.jsx
@@ -1,9 +1,26 @@
+import { Link, useParams } from "react-router-dom";
+import RecipeById from "../../Components/Recipe/RecipeById";
 
 export default function RecipePage() {
+    const { id } = useParams();
+
     return (
         <div style={{ padding: "1rem" }}>
-            <h2>Recipe Page</h2>
-            <p>This is where recipe details will go.</p>
+            <Link
+                to="/"
+                style={{
+                    color: "#60a5fa",
+                    textDecoration: "none",
+                    marginBottom: "1rem",
+                    display: "inline-block",
+                }}
+            >
+                ← Back to Search
+            </Link>
+
+            <RecipeById id={id} />
         </div>
     );
 }
+
+
