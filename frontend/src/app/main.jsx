27eee57babb87dca37cd6e2e36cdab80b2f2e1c1
Strongic <lstrong8@msudenver.edu>
2025-10-28 14:07:52 -0600

    removing excess duplicate files outside frontend/

--- frontend/src/app/main.jsx
+++ frontend/src/app/main.jsx
@@ -6,7 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/LetMeCook">
+    <BrowserRouter>
+      {/* <BrowserRouter basename="/LetMeCook"> */}
       <App />
     </BrowserRouter>
   </StrictMode>,
